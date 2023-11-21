b460b999300d03370166d6a6c2aa5881887e7743
Tom Slesinger <tom.slesinger@gmail.com>
2023-11-21 09:10:30 -0700

    Added box and custom styling.

--- wwwroot/js/custommoveable.js
+++ wwwroot/js/custommoveable.js
@@ -1,1 +1,8 @@
-console.log('test');
+const moveable = new Moveable(document.body, {
+    target: document.getElementById("root"), // Use getElementById instead of querySelector for ID selection
+    draggable: true,
+});
+
+moveable.on("drag", ({ target, transform }) => {
+    target.style.transform = transform;
+});
